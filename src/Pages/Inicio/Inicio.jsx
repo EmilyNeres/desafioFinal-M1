7d0b4aa72d286d8b2bffeac06d5a3d37f9c51e5d
Emily Neres <emilyneres31@gmail.com>
2024-11-22 19:36:21 -0300

    Initial commit

---
+++ src/Pages/Inicio/Inicio.jsx
@@ -0,0 +1,13 @@
+import S from'./inicio.module.scss'
+
+
+export default function Inicio(){
+    return(
+        <section className={S.post}>
+            <section>
+                <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+            </section>
+        </section>
+    )
+}
+
---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,10 @@
+
+
+export default function footer(){
+
+    return(
+        <footer>
+            <h1>Olá</h1>
+        </footer>
+    )
+}
