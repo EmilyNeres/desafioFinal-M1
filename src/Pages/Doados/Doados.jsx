6fe06af09a183e063da9c9c10981ac43b3590e00
Emily Neres <emilyneres31@gmail.com>
2024-11-29 15:41:25 -0300

    atualização da pagina Doados

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,9 +1,18 @@
-
+import S from './doados.module.scss'
+import oProtagonista from '../../assets/oProtagonista.png'
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxCard} >
+                <article>
+                    <img src={oProtagonista} alt="imagem do livro O Protagonista" />
+                    <h3>O protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
         </section>
     )
 }
